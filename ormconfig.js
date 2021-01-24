435e98a3d729dd09021360715dd12d3f24dec5ef
Luis Henrique <lh.p@hotmail.com>
2021-01-24 17:04:13 -0300

    Comments on ormconfig.

--- ormconfig.js
+++ ormconfig.js
@@ -3,13 +3,30 @@ module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
 
+  
+  /* ******************** */
   "entities": [
     "./dist/modules/**/infra/typeorm/entities/*.js"
   ],
 
+  // "entities": [
+  //   "./src/modules/**/infra/typeorm/entities/*.ts"
+  // ],
+  /* ******************** */
+
+
+  /* ******************** */
   "migrations": [
     "./dist/shared/infra/typeorm/migrations/*.js"
   ],
+
+  // "migrations": [
+  //   "./src/shared/infra/typeorm/migrations/*.ts"
+  // ],
+  /* ******************** */
+
+
+
   "cli": {
     "migrationsDir": "./src/shared/infra/typeorm/migrations"
   }
